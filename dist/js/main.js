fdc40906aef9b24fc46dd6c2b7033970a59cd088
Bozidar <bozidar.bralic@gmail.com>
2021-02-05 23:11:32 +0100

    Second Commit Pre Changing Rect Logic

--- dist/js/main.js
+++ dist/js/main.js
@@ -63,3 +63,27 @@ function init() {
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
 }
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+
+        const c = +counter.innerText;
+
+        const increment = target / 1000;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
